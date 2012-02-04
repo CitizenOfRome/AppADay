7ebcae7d5ca27036d7b95eaf58c0505222061ac9
Sathvik Ponangi <psathvik@gmail.com>
2012-02-04 21:02:58 +0530

    JNTU Bulk Downloader for a range for ecodes

--- JNTUH_Result_Downloader/jntuh_results_bulk_ecode.py
+++ JNTUH_Result_Downloader/jntuh_results_bulk_ecode.py
@@ -1,17 +1,15 @@
 #Python2.7.2
-'''Example: python jntu_results_bulk.py 08QQ1A04 1036 60 1'''
+'''Example: python jntu_results_bulk.py 08QQ1A0449 950 1050'''
 import sys
 import httplib
 import string
 
-if len(sys.argv)>4: start = int(sys.argv[4])
-else: start = 1
-if len(sys.argv)>3: std_max = int(sys.argv[3])
-else: std_max = 60
-if len(sys.argv)>2: ecode = sys.argv[2]
-else: ecode = "1036"
+if len(sys.argv)>3: end = int(sys.argv[3])
+else: end = 1040
+if len(sys.argv)>2: start = int(sys.argv[2])
+else: start = 980
 if len(sys.argv)>1: htno = sys.argv[1]
-else: htno = "08QQ1A04"
+else: htno = "08QQ1A0449"
 
 #print htno,ecode,std_max,start
 
@@ -48,10 +46,5 @@ def get_htno(htno, ecode):
     count = 0
     print data
 
-for i in xrange(start, std_max+1):
-    k = str(i)
-    z = 10-(len(htno)+len(k))
-    #print k, z
-    if z>=0: k = htno+("0"*z)+k
-    #print k
-    get_htno(k, ecode)
+for ecode in xrange(start, end+1):
+    get_htno(htno, str(ecode))
