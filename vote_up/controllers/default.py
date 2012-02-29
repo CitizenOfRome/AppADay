09de41841c11567251a9616ca99ed6cf545d6d5b
Sathvik Ponangi <psathvik@gmail.com>
2012-02-29 08:44:20 +0530

    Vote_up: Better UI

--- vote_up/controllers/default.py
+++ vote_up/controllers/default.py
@@ -6,14 +6,15 @@ session.path_to["default"] = "/"+request.application+"/default"
 
 def index():
     '''Display a post, its comments, its answers and allow for vote-up'''
+    if not session.user: return user()
     #print db(db.users).select()
     path_to=session.path_to
     posts=db(db.posts).select(orderby=(~db.posts.votes), limitby=(0,20))
-    if not session.user: return user()
     return response.render('default/main.html', locals())
     
 def get_post():
     '''Display a post, its comments, its answers and allow for vote-up'''
+    if not session.user: return user()
     path_to=session.path_to
     post=db(db.posts.id == request.args[0]).select().first()
     answers=db(db.answers.post == post).select(orderby=(~db.answers.votes), limitby=(0,20))
@@ -26,54 +27,80 @@ def get_post():
     
 def new_post():
     '''Accept a new post'''
+    if not request.vars.has_key("message"): return "false"
+    if not session.user: return user()
     path_to=session.path_to
     post_id = db.posts.insert(
         title = request.vars["title"],
-        message = request.vars["message"]
+        message = request.vars["message"],
+        user = session.user.id
     )
     #post = db.posts[post_id]
     return "true"
 
 def new_response():
     '''Accept a new response'''
+    if not request.vars.has_key("message"): return "false"
+    if not session.user: return user()
     path_to=session.path_to
     post=db(db.posts.id == request.args[0]).select().first()
     post_id = db.answers.insert(
         message = request.vars["message"],
-        post = post
+        post = post,
+        user = session.user.id
     )
     #post = db.posts[post_id]
     return "true"
 
 def new_comment():
     '''Accept a new comment for a post'''
+    if not request.vars.has_key("message"): return "false"
+    if not session.user: return user()
     path_to=session.path_to
     post=db(db.posts.id == request.args[0]).select().first()
+    if post.user!=session.user.id and session.user.reputation<20:    return json.dumps({"message":"You need atleast 20 rep to comment"})
     post_id = db.comments.insert(
         message = request.vars["message"],
-        post = post
+        post = post,
+        user = session.user.id
     )
     #post = db.posts[post_id]
     return "true"
 def new_comment_r():
     '''Accept a new comment for an answer'''
+    if not request.vars.has_key("message"): return "false"
+    if not session.user: return user()
     path_to=session.path_to
     answer=db(db.answers.id == request.args[0]).select().first()
+    if answer.user!=session.user.id and session.user.reputation<20:    return json.dumps({"message":"You need atleast 20 rep to comment"})
     db.comments_r.insert(
         message = request.vars["message"],
-        answer = answer
+        answer = answer,
+        user = session.user.id
     )
     return "true"
     
 def vote():
     '''Add a vote'''
-    if not session.user:    return user()
+    if not session.user:    return json.dumps({"votes":0, "status":0, "message":"You must be logged on to post"})
     path_to=session.path_to
-    if request.vars["post"]:    var = db(db.posts.id == request.vars["post"]).select().first()
-    elif request.vars["answer"]:    var = db(db.answers.id == request.vars["answer"]).select().first()
-    elif request.vars["comment"]:    var = db(db.comments.id == request.vars["comment"]).select().first()
-    elif request.vars["comment_r"]:    var = db(db.comments_r.id == request.vars["comment_r"]).select().first()
+    factor = 0
+    if request.vars["post"]:
+        var = db(db.posts.id == request.vars["post"]).select().first()
+        factor=5
+    elif request.vars["answer"]:
+        var = db(db.answers.id == request.vars["answer"]).select().first()
+        factor=10
+    elif request.vars["comment"]:
+        var = db(db.comments.id == request.vars["comment"]).select().first()
+        factor=2
+    elif request.vars["comment_r"]:
+        var = db(db.comments_r.id == request.vars["comment_r"]).select().first()
+        factor=2
     else: return None
+    if session.user.reputation<15:    return json.dumps({"votes":0, "status":0, "message":"You need atleast 15 rep to vote"})
+    user = db.users[var.user]
+    if user.id==session.user.id:  return json.dumps({"votes":var.votes, "status":0, "message":"You cannot vote on your own content"})
     inc = 0
     in2 = 0
     add = [session.user.id]
@@ -109,6 +136,9 @@ def vote():
             v_dn = var.v_dn+add,
             v_up = var.v_up
         )
+    user.update_record(
+        reputation = user.reputation+(inc+in2)*factor
+    )
     return json.dumps({"votes":var.votes, "status":inc})
     
 def user():
